737d82189dbff06882b08d2208c008eb25874d91
Vladimir Pazuhin <volt@SVETA.(none)>
2013-10-02 17:40:01 +0400

    ok

--- urq2sm.py
+++ urq2sm.py
@@ -9,8 +9,20 @@ def pln (text):
 	text = re.sub("^p ","", text)
 	text = re.sub(" p ","", text)
 	return text
+def object(text):
+	iflist = re.findall("if .*? then",text)	
+	for i in iflist:
+		newi = i[3:-5].replace(' ','_')
+		newi = newi.replace('_and_',' and ').replace("_or_"," or ").replace("not_","not ").replace('=','~')
+		text = text.replace(i,"if "+newi+" then")
+	iflist = re.findall("(then)*.*=.*",text)
+	for i in iflist:
+		newi = i.replace(' ','_')
+		newi = newi.replace('_=',' =').replace('=_','= ')
+		text = text.replace(i,newi)
+	return text
 def btn (text):
-	text = re.sub("btn (.*?),(.*?)\r\n","[[\\2|\\1]]\r\n",text)	
+	text = re.sub("btn (.*?), *(.*?)\r\n","[[\\2|\\1]]\r\n",text)	
 	return text
 def cls (text):
 	text = re.sub("cls\s","<<clrscr>>\r\n",text)
@@ -28,20 +40,19 @@ def ifthen (text):
 	iflist = re.findall("<<if .*?>>", text)
 	ifnewlist = []
 	for if1 in iflist:
-		 newif = re.sub("(\S+) *([\<\>\=])","$\\1\\2",if1[4:-2])
-		 newif = re.sub("([\<\>\=]) *(\D+)","\\1$\\2",newif)
+		 newif = re.sub("(\S+) *([\<\>\~])","$\\1\\2",if1[4:-2])
+		 newif = re.sub("([\<\>\~]) *(\D+)","\\1$\\2",newif)
 		 newif = newif.replace(" not "," ! ")
-		 newif = re.sub("(if|and|or|!) ([^[\$!]])","\\1 $\\2","<<if"+newif+">>")
-		 print newif
+		 newif = re.sub("(if|and|or|!) ([^\$^!])","\\1 $\\2","<<if"+newif+">>")		 
 		 ifnewlist.append(newif)
 	n = 0
 	for if1 in iflist:
-		ifnewlist[n] = "<<if" + ifnewlist[n][4:-2].replace('=',' eq ').replace('>',' gt ').replace('<',' lt ').replace(' $and ',' and ').replace(' $or ',' or ').replace(' $not ',' !')  + ">>"
+		ifnewlist[n] = "<<if" + ifnewlist[n][4:-2].replace('~',' eq ').replace('>',' gt ').replace('<',' lt ').replace(' $and ',' and ').replace(' $or ',' or ').replace(' $not ',' !')  + ">>"
 		text = text.replace(if1, ifnewlist[n])
 		n = n+1	
 	return text
 def set (text):
-	text = re.sub("(\S*?)=(.*)\r\n","<<set $\\1 = \\2>>\r\n",text)
+	text = re.sub("([^>\s=][^>=\n]+)=(.*)\r\n","<<set $\\1 = \\2>>\r\n",text)
 	return text
 def goto (text):
 	text = re.sub("goto (\S+)","<<display '\\1'>>",text)
@@ -62,6 +73,7 @@ def perkill (text):
 urqfile = open("Evgeny2.qst").read()
 smfile = open("hamster1.sm",'w')
 paragraph = re.compile("^:[\s\S]*?^end",re.MULTILINE)
+urqfile = object(urqfile)
 list_par = paragraph.findall(urqfile)
 resuilt = ""
 for par in list_par:
